65c903cb0609b7dc3ac7094884bedc4f32c66e05
Maseeha26 <maseeha26.razzack@gmail.com>
2020-02-21 19:36:49 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { AppComponent } from './app.component';
 import { StringInputComponent } from './string-input/string-input.component';
 import { RegexInputComponent } from
  './regex-input/regex-input.component';
-import { HighlightPipe } from './highlight.pipe';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, StringInputComponent, RegexInputComponent, HighlightPipe],
+  declarations: [ AppComponent, StringInputComponent, RegexInputComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
