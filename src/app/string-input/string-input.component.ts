2653d9b3834e031ebaf579eb9d8c2025cb621685
Maseeha26 <maseeha26.razzack@gmail.com>
2020-02-21 15:20:31 +0530

    4 files modified

--- src/app/string-input/string-input.component.ts
+++ src/app/string-input/string-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-string-input',
@@ -8,10 +10,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class StringInputComponent implements OnInit {
   public message = "A regular expression is a sequence of characters that forms a search pattern."
 
-  constructor() { }
+  stringForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
   @Output() stringEmit = new EventEmitter()
 
   ngOnInit() {
+      this.stringForm = this.fb.group({
+      stringValue: ['', Validators.required]
+    });
     this.stringEmit.emit(this.message);
   }
 
