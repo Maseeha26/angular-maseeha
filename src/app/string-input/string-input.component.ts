1ebf8f97b34da671b3a3cb2b2a57dd6f5356d404
Maseeha26 <maseeha26.razzack@gmail.com>
2020-02-21 12:32:01 +0530

    2 files modified

--- src/app/string-input/string-input.component.ts
+++ src/app/string-input/string-input.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./string-input.component.css']
 })
 export class StringInputComponent implements OnInit {
-
+public message = "Hello Code and Test World!";
   constructor() { }
   @Output() stringEmit = new EventEmitter()
   ngOnInit() {
+    this.stringEmit.emit(this.message);
   }
 
   sendString(message: string) {
